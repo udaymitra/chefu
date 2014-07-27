9e83f699e32499f26e50ef1d15a66675c1088bd2
rush00121 <rush00121@gmail.com>
2014-07-26 21:14:04 -0700

    Adding better implementation of UserDAO, added exec:java to pom.xml

--- src/main/java/com/chefu/resource/UserDAO.java
+++ src/main/java/com/chefu/resource/UserDAO.java
@@ -2,10 +2,11 @@ package com.chefu.resource;
 
 import com.chefu.model.UserAccount;
 import com.chefu.utils.ResourceHelper;
+import com.mongodb.BasicDBObject;
 import net.vz.mongodb.jackson.DBCursor;
 import net.vz.mongodb.jackson.JacksonDBCollection;
-import org.eclipse.jetty.util.log.Log;
-import org.eclipse.jetty.util.log.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -19,34 +20,37 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserDAO {
 
-    Logger log = Log.getLogger(UserDAO.class);
-    private JacksonDBCollection<UserAccount,String> users;
+    Logger log = LoggerFactory.getLogger(UserDAO.class);
+    private JacksonDBCollection<UserAccount, String> users;
 
     public UserDAO(JacksonDBCollection<UserAccount, String> users) {
         this.users = users;
     }
 
     @POST
-    public Response createUser(@Valid UserAccount userAccount){
+    public Response createUser(@Valid UserAccount userAccount) {
 
         DBCursor<UserAccount> cursor = users.find().is("emailAddress", userAccount.getEmailAddress());
         if (cursor.hasNext()) {
             return Response.status(Response.Status.CONFLICT).build();
         }
-        users.save(userAccount);
+        try {
+            users.insert(userAccount);
+        } catch (Exception e) {
+            log.error("Error inserting object in users database", e);
+            return Response.serverError().build();
+        }
         return Response.ok().build();
     }
 
     @GET
-    public UserAccount getUserAccount(@QueryParam("emailAddress") String email){
-        DBCursor<UserAccount> cursor = users.find().is("_id", email);
+    public UserAccount getUserAccount(@QueryParam("emailAddress") String email) {
+        DBCursor<UserAccount> cursor = users.find(new BasicDBObject("id", email)).is("_id", email);
         ResourceHelper.notFoundIfNull(cursor);
 
 
         return cursor.next();
     }
-
-
 
 
 }
