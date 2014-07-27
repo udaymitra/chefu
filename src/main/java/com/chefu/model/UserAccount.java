b745c9cfc8193099a8eb288837986924ce1842a3
rush00121 <rush00121@gmail.com>
2014-07-26 18:25:19 -0700

    Adding UserAccount to model

--- src/main/java/com/chefu/model/UserAccount.java
+++ src/main/java/com/chefu/model/UserAccount.java
@@ -1,7 +1,33 @@
 package com.chefu.model;
 
+import net.vz.mongodb.jackson.Id;
+import net.vz.mongodb.jackson.ObjectId;
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * Created by tanvirushabh on 7/26/14.
  */
 public class UserAccount {
+
+    @Id @ObjectId
+    String emailAddress;
+
+    @NotEmpty
+    String password ;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
